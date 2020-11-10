d72bf846b8dfe1d45fd2abe80fe278c37249e5f1
NoobSaiyan <ms19961998@gmail.com>
2020-11-10 16:49:38 +0530

    added info heading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import Scene from './components/scene.component'
+import Info from './components/info.component'
 import './App.css'
 
 export default function App() {
-  return <Scene />
+  return (
+    <div className='container'>
+      <div className='scene'>
+        <Scene />
+      </div>
+      <div className='info'>
+        <Info />
+      </div>
+    </div>
+  )
 }
---
+++ src/components/info.component.jsx
@@ -0,0 +1,10 @@
+import '../css/info.style.css'
+const Info = () => {
+  return (
+    <div className='base'>
+      <h1>NoobSaiyan</h1>
+      <hr />
+    </div>
+  )
+}
+export default Info
